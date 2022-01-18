06986dca23c7c58c0d6f9aeecc97253ed9a4c743
Ccasrol190604 <cristinacastroroldan@gmail.com>
2022-01-18 10:27:06 +0100

    Ejercicios capitulo 04

--- src/main/java/capitulo04_OrientacionObjetos/Ejercicio01_InstanciarOrdenadorJava.java
+++ src/main/java/capitulo04_OrientacionObjetos/Ejercicio01_InstanciarOrdenadorJava.java
@@ -5,13 +5,13 @@ public class Ejercicio01_InstanciarOrdenadorJava {
 	public static void main(String[] args) {
 		Ordenador java = new Ordenador();
 		
-		java.alturacm = 40;
-		java.anchuracm= 60;
-		java.color = "negro";
-		java.esPortatil = "sobremesa" != null ;
-		java.marca = "Fujitsu";
+		java.setAlturacm(40);
+		java.setAnchuracm(60);
+		java.setcolor("negro");
+		java.setEsPortatil("esPortatil");
+		java.setMarca("Fujitsu");
 		
-		java.ImprimePropiedades();
+		System.out.println(java.toString());
 	}
 
 }
--- src/main/java/capitulo04_OrientacionObjetos/Ordenador.java
+++ src/main/java/capitulo04_OrientacionObjetos/Ordenador.java
@@ -2,15 +2,60 @@ package capitulo04_OrientacionObjetos;
 
 public class Ordenador {
 
-	int Ordenador;
-	int alturacm;
-	int anchuracm;
-	String color;
-	boolean esPortatil;
-	String marca;
+	private int Ordenador;
+	private int alturacm;
+	private int anchuracm;
+	private String color;
+	private int esPortatil;
+	private String marca;
 	
-	public void ImprimePropiedades() {
-		System.out.println("Las propiedades son: alturacm: " + alturacm + " / anchuracm: " + anchuracm + " / color: " + color + 
-			" / esPortatil: " + esPortatil + " / marca: " + marca);
+	public String toString() {
+		return "Las propiedades son: alturacm: " + alturacm + " / anchuracm: " + anchuracm + " / color: " + color + 
+			" / esPortatil: " + esPortatil + " / marca: " + marca;
+	}
+	
+	public void setAlturacm (int newAlturacm) {
+		alturacm = newAlturacm;
+	}
+	
+	public int getAlturacm () {
+		return alturacm;
+	}
+	
+	public void setAnchuracm (int newAnchuracm) {
+		anchuracm = newAnchuracm;
+	}
+	
+	public int getAnchuracm () {
+		return anchuracm;
+	}
+	
+	public void setcolor (String string) {
+		color = string;
+	}
+	
+	public String getcolor () {
+		return color;
+	}
+	
+	public void setEsPortatil (int newEsPortatil) {
+		esPortatil = newEsPortatil;
+	}
+	
+	public int getesPortatil () {
+		return esPortatil;
+	}
+	
+	public void setMarca (String string) {
+		marca = string;
+	}
+	
+	public String getmarca () {
+		return marca;
+	}
+
+	public void setEsPortatil(String string) {
+		// TODO Auto-generated method stub
+		
 	}
 }
