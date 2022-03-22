f8e6da1a3b1e9053c84a87c80c0d99fffc808b99
Ccasrol190604 <cristinacastroroldan@gmail.com>
2022-03-22 10:31:09 +0100

    Capitulo 06

--- src/main/java/capitulo04_Ejemplo09_WindowBuilderCompleto/Ejercicio01_FormularioSuperhéroe.java
+++ src/main/java/capitulo04_Ejemplo09_WindowBuilderCompleto/Ejercicio01_FormularioSuperhéroe.java
@@ -16,12 +16,18 @@ import javax.swing.JPasswordField;
 import javax.swing.JCheckBox;
 import javax.swing.JButton;
 import javax.swing.JTextArea;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.SwingConstants;
 
 public class Ejercicio01_FormularioSuperhéroe {
 
 	private JFrame frame;
 	private JTextField textField;
 	private JPasswordField passwordField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -64,7 +70,7 @@ public class Ejercicio01_FormularioSuperhéroe {
 		gbl_panel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Gestión de clientes de criptomonedas");
+		JLabel lblNewLabel_1 = new JLabel("Formulario Superhéroe");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
 		gbc_lblNewLabel_1.gridwidth = 2;
@@ -83,14 +89,14 @@ public class Ejercicio01_FormularioSuperhéroe {
 		
 		textField = new JTextField();
 		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(10, 0, 5, 10);
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.insets = new Insets(10, 0, 5, 10);
 		gbc_textField.gridx = 1;
 		gbc_textField.gridy = 1;
 		panel.add(textField, gbc_textField);
 		textField.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("Tipo de crypto:");
+		JLabel lblNewLabel_2 = new JLabel("Apellidos:");
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
 		gbc_lblNewLabel_2.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_2.insets = new Insets(0, 0, 5, 5);
@@ -98,15 +104,16 @@ public class Ejercicio01_FormularioSuperhéroe {
 		gbc_lblNewLabel_2.gridy = 2;
 		panel.add(lblNewLabel_2, gbc_lblNewLabel_2);
 		
-		JComboBox comboBox = new JComboBox();
-		GridBagConstraints gbc_comboBox = new GridBagConstraints();
-		gbc_comboBox.insets = new Insets(0, 0, 5, 0);
-		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
-		gbc_comboBox.gridx = 1;
-		gbc_comboBox.gridy = 2;
-		panel.add(comboBox, gbc_comboBox);
+		textField_2 = new JTextField();
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.gridx = 1;
+		gbc_textField_2.gridy = 2;
+		panel.add(textField_2, gbc_textField_2);
+		textField_2.setColumns(10);
 		
-		JLabel lblNewLabel_3 = new JLabel("Clave privada:");
+		JLabel lblNewLabel_3 = new JLabel("NIF:");
 		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
 		gbc_lblNewLabel_3.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_3.insets = new Insets(0, 0, 5, 5);
@@ -122,56 +129,118 @@ public class Ejercicio01_FormularioSuperhéroe {
 		gbc_passwordField.gridy = 3;
 		panel.add(passwordField, gbc_passwordField);
 		
-		JLabel lblNewLabel_4 = new JLabel("Soy experto:");
+		JLabel lblNewLabel_4 = new JLabel("Apodo:");
 		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
+		gbc_lblNewLabel_4.anchor = GridBagConstraints.EAST;
 		gbc_lblNewLabel_4.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel_4.gridx = 0;
 		gbc_lblNewLabel_4.gridy = 4;
 		panel.add(lblNewLabel_4, gbc_lblNewLabel_4);
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("");
-		GridBagConstraints gbc_chckbxNewCheckBox = new GridBagConstraints();
-		gbc_chckbxNewCheckBox.insets = new Insets(0, 0, 5, 0);
-		gbc_chckbxNewCheckBox.anchor = GridBagConstraints.WEST;
-		gbc_chckbxNewCheckBox.gridx = 1;
-		gbc_chckbxNewCheckBox.gridy = 4;
-		panel.add(chckbxNewCheckBox, gbc_chckbxNewCheckBox);
+		textField_1 = new JTextField();
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.gridx = 1;
+		gbc_textField_1.gridy = 4;
+		panel.add(textField_1, gbc_textField_1);
+		textField_1.setColumns(10);
 		
-		JLabel lblNewLabel_5 = new JLabel("Observaciones:");
-		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
-		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel_5.gridx = 0;
-		gbc_lblNewLabel_5.gridy = 5;
-		panel.add(lblNewLabel_5, gbc_lblNewLabel_5);
+		JLabel lblNewLabel_4_1 = new JLabel("Sexo:");
+		GridBagConstraints gbc_lblNewLabel_4_1 = new GridBagConstraints();
+		gbc_lblNewLabel_4_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_4_1.anchor = GridBagConstraints.EAST;
+		gbc_lblNewLabel_4_1.gridx = 0;
+		gbc_lblNewLabel_4_1.gridy = 5;
+		panel.add(lblNewLabel_4_1, gbc_lblNewLabel_4_1);
 		
-		JTextArea textArea = new JTextArea();
-		GridBagConstraints gbc_textArea = new GridBagConstraints();
-		gbc_textArea.insets = new Insets(0, 0, 5, 0);
-		gbc_textArea.fill = GridBagConstraints.BOTH;
-		gbc_textArea.gridx = 1;
-		gbc_textArea.gridy = 5;
-		panel.add(textArea, gbc_textArea);
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setToolTipText("");
+		GridBagConstraints gbc_comboBox_1 = new GridBagConstraints();
+		gbc_comboBox_1.insets = new Insets(0, 0, 5, 0);
+		gbc_comboBox_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox_1.gridx = 1;
+		gbc_comboBox_1.gridy = 5;
+		panel.add(comboBox_1, gbc_comboBox_1);
 		
 		JPanel panel_1 = new JPanel();
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
 		gbc_panel_1.gridwidth = 2;
 		gbc_panel_1.insets = new Insets(0, 0, 0, 5);
-		gbc_panel_1.fill = GridBagConstraints.BOTH;
 		gbc_panel_1.gridx = 0;
 		gbc_panel_1.gridy = 6;
 		panel.add(panel_1, gbc_panel_1);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[] {0, 0, 0, 0, 0};
+		gbl_panel_1.rowHeights = new int[] {0, 0, 0};
+		gbl_panel_1.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0};
+		gbl_panel_1.rowWeights = new double[]{0.0, Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
 		
-		JButton btnNewButton = new JButton("Nuevo");
-		panel_1.add(btnNewButton);
+		JLabel lblNewLabel_5 = new JLabel("Vuelas: ");
+		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
+		gbc_lblNewLabel_5.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_5.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_5.gridx = 0;
+		gbc_lblNewLabel_5.gridy = 0;
+		panel_1.add(lblNewLabel_5, gbc_lblNewLabel_5);
 		
-		JButton btnNewButton_1 = new JButton("Guardar");
-		panel_1.add(btnNewButton_1);
+		JCheckBox chckbxNewCheckBox = new JCheckBox("");
+		GridBagConstraints gbc_chckbxNewCheckBox = new GridBagConstraints();
+		gbc_chckbxNewCheckBox.weightx = 1.0;
+		gbc_chckbxNewCheckBox.insets = new Insets(0, 0, 5, 5);
+		gbc_chckbxNewCheckBox.gridx = 1;
+		gbc_chckbxNewCheckBox.gridy = 0;
+		panel_1.add(chckbxNewCheckBox, gbc_chckbxNewCheckBox);
 		
-		JButton btnNewButton_2 = new JButton("Enviar");
-		panel_1.add(btnNewButton_2);
+		JCheckBox chckbxNewCheckBox_2 = new JCheckBox("");
+		GridBagConstraints gbc_chckbxNewCheckBox_2 = new GridBagConstraints();
+		gbc_chckbxNewCheckBox_2.weightx = 1.0;
+		gbc_chckbxNewCheckBox_2.insets = new Insets(0, 0, 5, 5);
+		gbc_chckbxNewCheckBox_2.gridx = 3;
+		gbc_chckbxNewCheckBox_2.gridy = 0;
+		panel_1.add(chckbxNewCheckBox_2, gbc_chckbxNewCheckBox_2);
 		
-		JButton btnNewButton_3 = new JButton("Eliminar");
-		panel_1.add(btnNewButton_3);
+		JLabel lblNewLabel_7 = new JLabel("Velocidad: ");
+		GridBagConstraints gbc_lblNewLabel_7 = new GridBagConstraints();
+		gbc_lblNewLabel_7.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel_7.gridx = 2;
+		gbc_lblNewLabel_7.gridy = 0;
+		panel_1.add(lblNewLabel_7, gbc_lblNewLabel_7);
+		
+		JLabel lblNewLabel_6 = new JLabel("Fuerza:");
+		GridBagConstraints gbc_lblNewLabel_6 = new GridBagConstraints();
+		gbc_lblNewLabel_6.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_6.gridx = 0;
+		gbc_lblNewLabel_6.gridy = 2;
+		panel_1.add(lblNewLabel_6, gbc_lblNewLabel_6);
+		
+		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("");
+		GridBagConstraints gbc_chckbxNewCheckBox_1 = new GridBagConstraints();
+		gbc_chckbxNewCheckBox_1.weightx = 1.0;
+		gbc_chckbxNewCheckBox_1.insets = new Insets(0, 0, 0, 5);
+		gbc_chckbxNewCheckBox_1.gridx = 1;
+		gbc_chckbxNewCheckBox_1.gridy = 2;
+		panel_1.add(chckbxNewCheckBox_1, gbc_chckbxNewCheckBox_1);
+		
+		JLabel lblNewLabel_8 = new JLabel("Rayos X: ");
+		GridBagConstraints gbc_lblNewLabel_8 = new GridBagConstraints();
+		gbc_lblNewLabel_8.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_8.gridx = 2;
+		gbc_lblNewLabel_8.gridy = 2;
+		panel_1.add(lblNewLabel_8, gbc_lblNewLabel_8);
+		
+		JCheckBox chckbxNewCheckBox_3 = new JCheckBox("");
+		GridBagConstraints gbc_chckbxNewCheckBox_3 = new GridBagConstraints();
+		gbc_chckbxNewCheckBox_3.weightx = 1.0;
+		gbc_chckbxNewCheckBox_3.anchor = GridBagConstraints.WEST;
+		gbc_chckbxNewCheckBox_3.insets = new Insets(0, 0, 0, 5);
+		gbc_chckbxNewCheckBox_3.gridx = 3;
+		gbc_chckbxNewCheckBox_3.gridy = 2;
+		panel_1.add(chckbxNewCheckBox_3, gbc_chckbxNewCheckBox_3);
+		
+		JButton btnNewButton = new JButton("Guardar");
+		frame.getContentPane().add(btnNewButton, BorderLayout.SOUTH);
 	}
 
 }
