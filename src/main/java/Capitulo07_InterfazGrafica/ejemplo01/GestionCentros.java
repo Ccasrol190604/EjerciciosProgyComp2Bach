d39d8d7892be98b0af1ce0495e8f183d782f5050
Ccasrol190604 <cristinacastroroldan@gmail.com>
2022-04-19 10:37:06 +0200

    Gestión Centros

--- src/main/java/Capitulo07_InterfazGrafica/ejemplo01/GestionCentros.java
+++ src/main/java/Capitulo07_InterfazGrafica/ejemplo01/GestionCentros.java
@@ -75,14 +75,14 @@ public class GestionCentros {
 		gbc_lblNewLabel.gridy = 0;
 		frame.getContentPane().add(lblNewLabel, gbc_lblNewLabel);
 		
-		textField = new JTextField();
+		jtfid = new JTextField();
 		GridBagConstraints gbc_textField = new GridBagConstraints();
 		gbc_textField.insets = new Insets(0, 0, 5, 0);
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField.gridx = 1;
 		gbc_textField.gridy = 0;
-		frame.getContentPane().add(textField, gbc_textField);
-		textField.setColumns(10);
+		frame.getContentPane().add(jtfid, gbc_textField);
+		jtfid.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Tipo");
 		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
@@ -92,14 +92,14 @@ public class GestionCentros {
 		gbc_lblNewLabel_1.gridy = 1;
 		frame.getContentPane().add(lblNewLabel_1, gbc_lblNewLabel_1);
 		
-		textField_1 = new JTextField();
+		jtftipo = new JTextField();
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
 		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_1.gridx = 1;
 		gbc_textField_1.gridy = 1;
-		frame.getContentPane().add(textField_1, gbc_textField_1);
-		textField_1.setColumns(10);
+		frame.getContentPane().add(jtftipo, gbc_textField_1);
+		jtftipo.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Denominación");
 		GridBagConstraints gbc_lblNewLabel_2 = new GridBagConstraints();
@@ -109,14 +109,14 @@ public class GestionCentros {
 		gbc_lblNewLabel_2.gridy = 2;
 		frame.getContentPane().add(lblNewLabel_2, gbc_lblNewLabel_2);
 		
-		textField_2 = new JTextField();
+		jtfdenominacion = new JTextField();
 		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
 		gbc_textField_2.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_2.gridx = 1;
 		gbc_textField_2.gridy = 2;
-		frame.getContentPane().add(textField_2, gbc_textField_2);
-		textField_2.setColumns(10);
+		frame.getContentPane().add(jtfdenominacion, gbc_textField_2);
+		jtfdenominacion.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Dirección");
 		GridBagConstraints gbc_lblNewLabel_3 = new GridBagConstraints();
@@ -126,14 +126,14 @@ public class GestionCentros {
 		gbc_lblNewLabel_3.gridy = 3;
 		frame.getContentPane().add(lblNewLabel_3, gbc_lblNewLabel_3);
 		
-		textField_3 = new JTextField();
+		jtfdireccion = new JTextField();
 		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
 		gbc_textField_3.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_3.gridx = 1;
 		gbc_textField_3.gridy = 3;
-		frame.getContentPane().add(textField_3, gbc_textField_3);
-		textField_3.setColumns(10);
+		frame.getContentPane().add(jtfdireccion, gbc_textField_3);
+		jtfdireccion.setColumns(10);
 		
 		JLabel lblNewLabel_4 = new JLabel("Población");
 		GridBagConstraints gbc_lblNewLabel_4 = new GridBagConstraints();
@@ -143,14 +143,14 @@ public class GestionCentros {
 		gbc_lblNewLabel_4.gridy = 4;
 		frame.getContentPane().add(lblNewLabel_4, gbc_lblNewLabel_4);
 		
-		textField_4 = new JTextField();
+		jtfpoblacion = new JTextField();
 		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
 		gbc_textField_4.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_4.gridx = 1;
 		gbc_textField_4.gridy = 4;
-		frame.getContentPane().add(textField_4, gbc_textField_4);
-		textField_4.setColumns(10);
+		frame.getContentPane().add(jtfpoblacion, gbc_textField_4);
+		jtfpoblacion.setColumns(10);
 		
 		JLabel lblNewLabel_5 = new JLabel("Provincia");
 		GridBagConstraints gbc_lblNewLabel_5 = new GridBagConstraints();
@@ -160,14 +160,14 @@ public class GestionCentros {
 		gbc_lblNewLabel_5.gridy = 5;
 		frame.getContentPane().add(lblNewLabel_5, gbc_lblNewLabel_5);
 		
-		textField_5 = new JTextField();
+		jtfprovincia = new JTextField();
 		GridBagConstraints gbc_textField_5 = new GridBagConstraints();
 		gbc_textField_5.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_5.gridx = 1;
 		gbc_textField_5.gridy = 5;
-		frame.getContentPane().add(textField_5, gbc_textField_5);
-		textField_5.setColumns(10);
+		frame.getContentPane().add(jtfprovincia, gbc_textField_5);
+		jtfprovincia.setColumns(10);
 		
 		JPanel panel = new JPanel();
 		GridBagConstraints gbc_panel = new GridBagConstraints();
@@ -256,9 +256,11 @@ public class GestionCentros {
 		   
 			if (rs.next() == true) { 
 				jtfid.setText(rs.getString("id"));
-				jtftipo.setText(rs.getString("Nombre"));
-				jtfapellidos.setText(rs.getString("Apellidos"));
-				jtfNIF.setText(rs.getString("NIF"));
+				jtftipo.setText(rs.getString("Tipo"));
+				jtfdenominacion.setText(rs.getString("Denominación"));
+				jtfdireccion.setText(rs.getString("Dirección"));
+				jtfpoblacion.setText(rs.getString("Población"));
+				jtfprovincia.setText(rs.getString("Provincia"));
 			}
 			rs.close();
 			s.close();
